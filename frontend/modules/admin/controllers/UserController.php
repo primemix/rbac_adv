0415f6134a2940511ae34ff93d812113649a2f4e
Serhii Kosyouk <monstrpro@gmail.com>
2018-01-11 09:33:36 +0200

    Add UserController.php

--- frontend/modules/admin/controllers/UserController.php
+++ frontend/modules/admin/controllers/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
                 'rules' => [
                     [
                         'allow' => true,
-                        'roles' => ['Логистик']
+                        'roles' => ['Администратор']
                     ],
                 ],
             ],
